e65a0c5fae95d5d4d27c6b0530a22f587fa9c87a
Daniel Ruoso <daniel@ruoso.com>
2025-06-16 05:30:51 -0400

    make dendrite functions inline

--- src/libs/neuronlib/dendrite.h
+++ src/libs/neuronlib/dendrite.h
@@ -18,13 +18,36 @@ constexpr uint32_t BRANCH_L2_MASK = 0x1C0;   // Bits 6-8
 constexpr uint32_t BRANCH_L3_MASK = 0xE00;   // Bits 9-11
 constexpr uint32_t DENDRITE_MASK = 0xFFF;    // Last 12 bits
 
-bool is_terminal_address(uint32_t address);
-bool is_branch_address(uint32_t address);
-bool is_neuron_address(uint32_t address);
+inline bool is_terminal_address(uint32_t address) {
+    return (address & TERMINAL_MASK) != 0;
+}
 
-uint32_t get_terminal_branch(uint32_t terminal_address);
-uint32_t get_parent_branch(uint32_t branch_address);
-uint32_t get_neuron_address(uint32_t dendrite_address);
+inline bool is_branch_address(uint32_t address) {
+    return (address & TERMINAL_MASK) == 0 && (address & DENDRITE_MASK) != 0;
+}
+
+inline bool is_neuron_address(uint32_t address) {
+    return (address & DENDRITE_MASK) == 0;
+}
+
+inline uint32_t get_terminal_branch(uint32_t terminal_address) {
+    return terminal_address & ~TERMINAL_MASK;
+}
+
+inline uint32_t get_parent_branch(uint32_t branch_address) {
+    if ((branch_address & BRANCH_L1_MASK) != 0) {
+        return branch_address & ~BRANCH_L1_MASK;
+    } else if ((branch_address & BRANCH_L2_MASK) != 0) {
+        return branch_address & ~BRANCH_L2_MASK;
+    } else if ((branch_address & BRANCH_L3_MASK) != 0) {
+        return branch_address & ~BRANCH_L3_MASK;
+    }
+    return branch_address;
+}
+
+inline uint32_t get_neuron_address(uint32_t dendrite_address) {
+    return dendrite_address & ~DENDRITE_MASK;
+}
 
 } // namespace neuronlib
 
